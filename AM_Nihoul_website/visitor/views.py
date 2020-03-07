b9b01d67a93a666a848441fd178d32c367b6b48e
Pierre Beaujean <pierreb24@gmail.com>
2020-03-07 22:12:02 +0100

    view upload

--- AM_Nihoul_website/visitor/views.py
+++ AM_Nihoul_website/visitor/views.py
@@ -1,8 +1,8 @@
 import flask
-from flask import Blueprint
+from flask import Blueprint, views
 
 from AM_Nihoul_website.base_views import RenderTemplateView, BaseMixin, ObjectManagementMixin
-from AM_Nihoul_website.visitor.models import Page
+from AM_Nihoul_website.visitor.models import Page, UploadedFile
 
 visitor_blueprint = Blueprint('visitor', __name__)
 
@@ -33,3 +33,37 @@ class PageView(BaseMixin, ObjectManagementMixin, RenderTemplateView):
 
 
 visitor_blueprint.add_url_rule('/page/<int:id>-<string:slug>.html', view_func=PageView.as_view(name='page-view'))
+
+
+class UploadView(views.View):
+    methods = ['GET']
+
+    def get(self, *args, **kwargs):
+        f = UploadedFile.query\
+            .filter(UploadedFile.file_name == kwargs.get('filename'))\
+            .filter(UploadedFile.id == kwargs.get('id'))\
+            .all()
+
+        if len(f) < 1:
+            flask.abort(404)
+
+        f = f[0]
+
+        with open(f.path(), 'rb') as fx:
+            response = flask.make_response(fx.read())
+
+        response.headers['Content-Disposition'] = 'attachment; filename={}'.format(f.file_name)
+        response.headers['Cache-Control'] = 'must-revalidate'
+        response.headers['Pragma'] = 'must-revalidate'
+        response.headers['Content-type'] = f.possible_mime
+
+        return response
+
+    def dispatch_request(self, *args, **kwargs):
+        if flask.request.method == 'GET':
+            return self.get(*args, **kwargs)
+        else:
+            flask.abort(403)
+
+
+visitor_blueprint.add_url_rule('/fichier/<int:id>/<string:filename>', view_func=UploadView.as_view(name='upload-view'))
