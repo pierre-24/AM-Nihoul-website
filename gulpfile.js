2f5a77a6dad69980500545f209958cc34011b738
Pierre Beaujean <pierreb24@gmail.com>
2020-09-23 12:16:41 +0200

    custom form (fix #4)

--- gulpfile.js
+++ gulpfile.js
@@ -16,16 +16,8 @@ if (fast) {
     console.log("no minification will be performed");
 }
 
-function js_editor() {
-    return browserify(input_dir + 'editor.js')
-        .bundle()
-        .pipe(gulp_if(!fast, minify()))
-        .pipe(source('editor.bundled.js'))
-        .pipe(gulp.dest(output_dir))
-}
-
-function css2() {
-    return gulp.src(input_dir + 'style2.less')
+function css() {
+    return gulp.src(input_dir + 'style.less')
         .pipe(less())
         .pipe(gulp_if(!fast, clean_css()))
         .pipe(gulp.dest(output_dir))
@@ -36,12 +28,8 @@ function images() {
         .pipe(gulp.dest(output_dir + 'images/'))
 }
 
-gulp.task('js_editor', function () {
-    return js_editor();
-});
-
-gulp.task('css2', function () {
-    return css2();
+gulp.task('css', function () {
+    return css();
 });
 
 gulp.task('images', function () {
@@ -49,12 +37,11 @@ gulp.task('images', function () {
 });
 
 function watch() {
-    gulp.watch([input_dir + 'editor.js'], {}, gulp.series('js_editor'));
-    gulp.watch([input_dir + 'style2.less'], {}, gulp.series('css'));
+    gulp.watch([input_dir + 'style.less'], {}, gulp.series('css'));
     gulp.watch([input_dir + 'images/*'], {}, gulp.series('images'));
 }
 
-gulp.task('build', gulp.parallel('css2', 'images', 'js_editor'));
+gulp.task('build', gulp.parallel('css', 'images'));
 gulp.task('default', gulp.series('build'));
 
 exports.watch = gulp.series('build', watch);
